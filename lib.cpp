2e7e07d15bcd7dc271d0fd8a5ff6c6f40e3929f5
Andrea-Fiori <115191307+Andrea-Fiori@users.noreply.github.com>
2022-12-05 13:27:58 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,19 @@
-
+#include "lib.h"
+bool Ver(char a){
+    int b;
+    b=(int)a;
+    if(65<=b && b<=90)
+        return true;
+    else if (97<=b && b<=122)
+        return true;
+    else
+        return false;
+}
+char Conv (char a){
+    char b;
+    if (((int) a)<=90 && ((int) a)>=65)
+    b= ((int (a)+32));
+    else if (((int) a)<= 122 && ((int) a) >= 97)
+        b= ((int (a)-32));
+    return b;
+}
