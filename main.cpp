63791f283a23a04a988fab28a663f0c412a0b551
Andrea-Fiori <115191307+Andrea-Fiori@users.noreply.github.com>
2022-12-05 13:29:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
 int main(){
+   char a=0;
+    cin >> a;
+    if (Ver(a)){
+       cout <<  Conv(a);
+    }else
+        cout << "errore";
+
   return 0;
 }
